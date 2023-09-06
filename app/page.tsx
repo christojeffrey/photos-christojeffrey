17daeb0264bd26f0a56cd3bd0aa866f4737ee4cf
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2023-09-06 18:43:35 -0400

    feat: adding photos
    Former-commit-id: 4d419c47fa873cc0d3a7d8585158c74b7aeb0384

--- app/page.tsx
+++ app/page.tsx
@@ -4,23 +4,107 @@ import Image from "next/image";
 const images = [
   {
     src: "/airplane-window.jpg",
-    alt: "test",
+    alt: "airplane-window",
   },
   {
     src: "/japan-car.jpg",
-    alt: "test",
+    alt: "japan airport car",
   },
   {
     src: "/sidewalk-sit.jpg",
-    alt: "test",
+    alt: "sitting on sidewalk",
   },
   {
     src: "/sun-kissed.jpg",
-    alt: "test",
+    alt: "sun kissed",
   },
   {
-    src: "/airplane-window.jpg",
-    alt: "test",
+    src: "/man-airport.jpg",
+    alt: "man at airport",
+  },
+  {
+    src: "/man-airport.jpg",
+    alt: "man at airport",
+  },
+  {
+    src: "/bus-stop.jpg",
+    alt: "bus stop broad daylight",
+  },
+  {
+    src: "/pedestrian-daylight.jpg",
+    alt: "pedestrian daylight",
+  },
+  {
+    src: "/bus-window.jpg",
+    alt: "bus window",
+  },
+  {
+    src: "/night-satay.jpg",
+    alt: "night satay",
+  },
+  {
+    src: "/girl-school-aisle.jpg",
+    alt: "a girl standing at the end of an aisle",
+  },
+  {
+    src: "/car-window.jpg",
+    alt: "sun kissed car window",
+  },
+  {
+    src: "/cold-night.jpg",
+    alt: "cold night",
+  },
+  {
+    src: "/random-pink-flowers.jpg",
+    alt: "sun kissed car window",
+  },
+  {
+    src: "/top-light-school.jpg",
+    alt: "top light school",
+  },
+  {
+    src: "/indonesian-flag-gbk.jpg",
+    alt: "indonesian-flag-gbk jakarta",
+  },
+  {
+    src: "/top-light-school.jpg",
+    alt: "top light school",
+  },
+  {
+    src: "/waterloo-downtown.jpg",
+    alt: "waterloo-downtown",
+  },
+  {
+    src: "/blue-school.jpg",
+    alt: "blue school",
+  },
+  {
+    src: "/cold-night-2.jpg",
+    alt: "another cold night",
+  },
+  {
+    src: "/golden-store.jpg",
+    alt: "goldern store",
+  },
+  {
+    src: "/waterloo-city.jpg",
+    alt: "waterloo city",
+  },
+  {
+    src: "/gbk-light.jpg",
+    alt: "gbk light",
+  },
+  {
+    src: "/google-building-waterloo.jpg",
+    alt: "google building in waterloo",
+  },
+  {
+    src: "/bees-on-sweet-drink.jpg",
+    alt: "bees on sweet drink",
+  },
+  {
+    src: "/cold-night-further.jpg",
+    alt: "cold night further",
   },
 ];
 
