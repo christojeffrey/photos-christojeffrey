8ed812835385bcc02f2b7791916fb8da07547875
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2023-09-06 12:21:12 -0400

    feat: making it responsive
    Former-commit-id: ad4343e5b8ebce3d22b494c43c33cc4344ca1762

--- app/page.tsx
+++ app/page.tsx
@@ -28,15 +28,15 @@ export default function Home() {
   return (
     <main className="flex md:flex-row min-h-screen flex-col items-start justify-between p-6">
       {/* sidebar */}
-      <div className="top-6 sticky w-[250px]">
+      <div className="top-6 sticky w-[200px]">
         <div className="p-5">photos by Christopher Jeffrey</div>
       </div>
       {/* photos */}
-      <div className="w-full columns-4 gap-0">
+      <div className="w-full columns-1 md:columns-2 lg:columns-4 gap-0">
         {images.map(async (image, index) => {
           const base64 = await getBase64(image.src);
           return (
-            <div key={index} className="rounded-lg relative p-6">
+            <div key={index} className="rounded-lg relative p-1 md:p-2 lg:p-4">
               <Image src={image.src} alt={image.alt} placeholder="blur" blurDataURL={base64} style={{ width: "100%", height: "auto" }} width={0} height={0} sizes="100vw" className="rounded-lg" />
             </div>
           );
