58b97db974531f8fd4822cad4a97408d80071618
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2023-09-09 15:08:09 -0400

    feat: add lazy loading
    Former-commit-id: bd7e2460f8a201a2bb7a56743958c8c31957cf46

--- app/page.tsx
+++ app/page.tsx
@@ -1,106 +1,7 @@
 import { getBase64 } from "@/lib";
 import Image from "next/image";
 
-const images = [
-  {
-    src: "/airplane-window.jpg",
-    alt: "airplane-window",
-  },
-  {
-    src: "/japan-car.jpg",
-    alt: "japan airport car",
-  },
-  {
-    src: "/sidewalk-sit.jpg",
-    alt: "sitting on sidewalk",
-  },
-  {
-    src: "/sun-kissed.jpg",
-    alt: "sun kissed",
-  },
-  {
-    src: "/man-airport.jpg",
-    alt: "man at airport",
-  },
-
-  {
-    src: "/bus-stop.jpg",
-    alt: "bus stop broad daylight",
-  },
-  {
-    src: "/pedestrian-daylight.jpg",
-    alt: "pedestrian daylight",
-  },
-  {
-    src: "/bus-window.jpg",
-    alt: "bus window",
-  },
-  {
-    src: "/night-satay.jpg",
-    alt: "night satay",
-  },
-  {
-    src: "/girl-school-aisle.jpg",
-    alt: "a girl standing at the end of an aisle",
-  },
-  {
-    src: "/car-window.jpg",
-    alt: "sun kissed car window",
-  },
-  {
-    src: "/cold-night.jpg",
-    alt: "cold night",
-  },
-  {
-    src: "/random-pink-flowers.jpg",
-    alt: "sun kissed car window",
-  },
-  {
-    src: "/top-light-school.jpg",
-    alt: "top light school",
-  },
-  {
-    src: "/indonesian-flag-gbk.jpg",
-    alt: "indonesian-flag-gbk jakarta",
-  },
-
-  {
-    src: "/waterloo-downtown.jpg",
-    alt: "waterloo-downtown",
-  },
-  {
-    src: "/blue-school.jpg",
-    alt: "blue school",
-  },
-  {
-    src: "/cold-night-2.jpg",
-    alt: "another cold night",
-  },
-  {
-    src: "/golden-store.jpg",
-    alt: "goldern store",
-  },
-  {
-    src: "/waterloo-city.jpg",
-    alt: "waterloo city",
-  },
-  {
-    src: "/gbk-light.jpg",
-    alt: "gbk light",
-  },
-  {
-    src: "/google-building-waterloo.jpg",
-    alt: "google building in waterloo",
-  },
-  {
-    src: "/bees-on-sweet-drink.jpg",
-    alt: "bees on sweet drink",
-  },
-  {
-    src: "/cold-night-further.jpg",
-    alt: "cold night further",
-  },
-];
+import images from "@/images";
 
 export default function Home() {
   return (
@@ -115,7 +16,7 @@ export default function Home() {
           const base64 = await getBase64(image.src);
           return (
             <div key={index} className="rounded-lg relative p-1 md:p-2 lg:p-4">
-              <Image src={image.src} alt={image.alt} placeholder="blur" blurDataURL={base64} style={{ width: "100%", height: "auto" }} width={0} height={0} sizes="100vw" className="rounded-lg" />
+              <Image src={image.src} alt={image.alt} placeholder="blur" blurDataURL={base64} style={{ width: "100%", height: "auto" }} width={0} height={0} sizes="100vw" className="rounded-lg" loading="lazy" />
             </div>
           );
         })}
