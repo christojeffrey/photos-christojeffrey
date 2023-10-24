285c89d52c0b8f8957d453fb3f6ca85ad994cb3d
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2023-10-23 23:03:48 -0400

    feat: seems working using css, even after build and start locally. need to compress images bellow 2mb
    Former-commit-id: 51a101846cb34c8f4ffd240a98f51e540f63f7c5

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <div className="p-5">photos by Christopher Jeffrey</div>
       </div>
       {/* photos */}
-      <div className="w-full columns-1 md:columns-2 lg:columns-4 gap-0">
+      <div className="w-full columns-1 md:columns-2 lg:columns-4 gap-1 md:gap-2 lg:gap-4 ">
         {images.reverse().map(async (image, index) => {
           // const base64 = await getBase64(image);
           const css = await getCSS(image);
@@ -21,8 +21,9 @@ export default function Home() {
             return <>error</>;
           }
           return (
-            <div key={index} className="rounded-lg relative p-1 md:p-2 lg:p-4">
-              <Image src={image} alt="image" style={{ width: "100%", height: "auto", ...css }} placeholder="blur" width={0} height={0} sizes="100vw" className="rounded-lg" priority loading="eager" />
+            <div key={index} className="rounded-lg relative overflow-hidden mb-1 md:mb-2 lg:mb-4">
+              <div className="absolute inset-0 transform scale-150 filter blur-2xl z-[-1]" style={css} />
+              <Image src={image} alt="image" style={{ width: "100%", height: "auto" }} width={0} height={0} sizes="100vw" className="rounded-lg" priority />
             </div>
           );
         })}
