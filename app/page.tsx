5d6607575722211818a43f6cd5f63f409ac81b94
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2023-10-23 21:40:52 -0400

    test: changing it to css plaiceholder
    Former-commit-id: 6a2bb5a00d619ffc11a72c00f84196b16400e6e6

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { getBase64 } from "@/lib";
+import { getBase64, getCSS } from "@/lib";
 import Image from "next/image";
 
 import images from "@/images";
@@ -13,10 +13,16 @@ export default function Home() {
       {/* photos */}
       <div className="w-full columns-1 md:columns-2 lg:columns-4 gap-0">
         {images.reverse().map(async (image, index) => {
-          const base64 = await getBase64(image);
+          // const base64 = await getBase64(image);
+          const css = await getCSS(image);
+
+          // if css string, return error
+          if (typeof css === "string") {
+            return <>error</>;
+          }
           return (
             <div key={index} className="rounded-lg relative p-1 md:p-2 lg:p-4">
-              <Image src={image} alt="image" placeholder="blur" blurDataURL={base64} style={{ width: "100%", height: "auto" }} width={0} height={0} sizes="100vw" className="rounded-lg" loading="lazy" quality={30} />
+              <Image src={image} alt="image" style={{ width: "100%", height: "auto", ...css }} placeholder="blur" width={0} height={0} sizes="100vw" className="rounded-lg" priority loading="eager" />
             </div>
           );
         })}
--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,18 @@
+import { getPlaiceholder } from "plaiceholder";
+
+export async function getCSS(fileName: string) {
+  try {
+    const src = fileName;
+
+    const buffer = await fetch(src, { cache: "no-store" }).then(async (res) => Buffer.from(await res.arrayBuffer()));
+
+    const { css } = await getPlaiceholder(buffer);
+
+    return css;
+  } catch (err) {
+    err;
+    return "error";
+  }
+}
+
 export { getBase64 } from "./getBase64";
