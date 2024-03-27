a296ea66ca04cd32c03700e12ff3058566ac1eca
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2024-03-28 02:51:06 +0700

    add cors respond

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -4,5 +4,12 @@ import { revalidateTag } from "next/cache";
 export async function GET(request: NextRequest) {
   const tag: any = request.nextUrl.searchParams.get("tag");
   revalidateTag(tag);
-  return Response.json({ revalidated: true, now: Date.now(), tag: tag });
+  return new Response(JSON.stringify({ revalidated: true, now: Date.now(), tag: tag }), {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  });
 }
